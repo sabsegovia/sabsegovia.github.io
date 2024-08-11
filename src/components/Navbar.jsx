b69ab5d18d126d29091e5793e8a2909cdb350aff
sabsegovia <ssabbsegovia@gmail.com>
2024-08-11 19:07:31 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import '../styles/Navbar.css';
+import '../index.css';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <NavLink to="/" exact activeClassName="active-link" className="nav-link">
-          Home
-        </NavLink>
-                <NavLink to="/watchlist" activeClassName="active-link" className="nav-link">
-          Watchlist
-        </NavLink>
-        <NavLink to="/favorites" activeClassName="active-link" className="nav-link">
-          Favoritos
-        </NavLink>
-        <NavLink to="/watched" activeClassName="active-link" className="nav-link">
-          Watched
-        </NavLink>
+        <button className="navbar-toggle" onClick={toggleNavbar}>
+          &#9776; {}
+        </button>
+        <div className={`navbar-links ${isOpen ? 'open' : ''}`}>
+          <NavLink to="/" exact activeClassName="active-link" className="nav-link">
+            Home
+          </NavLink>
+          <NavLink to="/watchlist" activeClassName="active-link" className="nav-link">
+            Watchlist
+          </NavLink>
+          <NavLink to="/favorites" activeClassName="active-link" className="nav-link">
+            Favoritos
+          </NavLink>
+          <NavLink to="/watched" activeClassName="active-link" className="nav-link">
+            Watched
+          </NavLink>
+        </div>
       </div>
     </nav>
   );
 };
+
+export default Navbar;
+
 
 export default Navbar;
