e0052063c44fb9ab823381017ff67587d58b041a
sabsegovia <ssabbsegovia@gmail.com>
2024-08-11 19:06:20 -0300

    Update FavoritesPage.jsx

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/FavoritesPage.css';
 import { movies } from '../data/movies';
 import MovieCard from '../components/MovieCard';
+import '../index.css';
 
 const FavoritesPage = ({ favorites, handleListChange, handleDeleteMovie }) => {
   return (
